3d1a0f5317f39fc583ef418681782ead83b0083c
Gilson Cavalcanti <gilsoncav@gmail.com>
2019-11-30 13:34:23 -0300

    adequating spaces to prettier defaults

--- js/script.js
+++ js/script.js
@@ -2,9 +2,8 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 /*** 
    Add your global variables that store the DOM elements you will 
@@ -16,11 +15,9 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
+const studentListItems = document.querySelector('.student-list').children;
 
-
-
-
-/*** 
+/***  
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
 
@@ -35,16 +32,9 @@ FSJS project 2 - List Filter and Pagination
        "invoke" the function 
 ***/
 
-
-
-
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
-
-
-
-
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
