1ce0c4190479931ea6922cc7e97af316fdd6e918
Gilson Cavalcanti <gilsoncav@gmail.com>
2019-11-30 16:46:40 -0300

    deleted existing and added my own comments

--- js/script.js
+++ js/script.js
@@ -1,40 +1,29 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+/***
+ * GLOBAL VARIABLES
+ */
 
-/*** 
-  Add your global variables that store the DOM elements you will 
-  need to reference and/or manipulate. 
-
-  But be mindful of which variables should be global and which 
-  should be locally scoped to one of the two main functions you're 
-  going to create. A good general rule of thumb is if the variable 
-  will only be used inside of a function, then it can be locally 
-  scoped to that function.
-***/
+// Array containing all students list items
 const studentListItems = Array.from(
   document.querySelector('.student-list').children
 );
+// Number of items to show in each page
 const kNumberItemsPerPage = 10;
 
-/***  
-  Create the `showPage` function to hide all of the items in the 
-  list except for the ten you want to show.
-
-  Pro Tips: 
-    - Keep in mind that with a list of 54 students, the last page 
-    will only display four.
-    - Remember that the first student has an index of 0.
-    - Remember that a function `parameter` goes in the parens when 
-    you initially define the function, and it acts as a variable 
-    or a placeholder to represent the actual function `argument` 
-    that will be passed into the parens later when you call or 
-    "invoke" the function 
-***/
+/**
+ * Shows just a particular page of the student list paginated by
+ * `kNumberItemsPerPage` items per page.
+ *
+ * @param {Array<HTMLLIElement>} list the array with all students lines
+ * @param {number} page the number os the pagination to show
+ */
 function showPage(list, page) {
+  hideAll();
   const lastPage = page === totalPages();
   const startIndex = kNumberItemsPerPage * (page - 1);
   let endIndex = !lastPage
@@ -46,19 +35,24 @@ function showPage(list, page) {
   }
 }
 
-const hideAll = list => {
-  list.forEach(li => (li.style.display = 'none'));
-};
+/**
+ * A helper function to hide all students list items
+ */
+function hideAll() {
+  studentListItems.forEach(li => (li.style.display = 'none'));
+}
 
+/**
+ * A helper funtion to calculate the total number of pagination pages
+ */
 function totalPages() {
   return Math.ceil(studentListItems.length / kNumberItemsPerPage);
 }
 
-/*** 
-  Create the `appendPageLinks function` to generate, append, and add 
-  functionality to the pagination buttons.
-***/
-
+/**
+ * Generates the bottom screen element that allows the user to choose wich page
+ * to display
+ */
 function appendPageLinks() {
   // creating the containers tags...
   const divPagination = document.createElement('div');
@@ -72,6 +66,7 @@ function appendPageLinks() {
     const a = document.createElement('a');
     a.href = '#';
     a.textContent = i + 1;
+    if (i === 0) a.className = 'active';
     li.appendChild(a);
     ulPagesButtons.appendChild(li);
   }
@@ -84,12 +79,11 @@ function appendPageLinks() {
       );
       allLinks.forEach(a => (a.className = ''));
       e.target.className = 'active';
+      showPage(studentListItems, parseInt(e.target.textContent));
     }
   });
 }
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
-
-hideAll(studentListItems);
-showPage(studentListItems, 6);
+// By default at the first load of the script, show the first page
+showPage(studentListItems, 1);
 appendPageLinks();
